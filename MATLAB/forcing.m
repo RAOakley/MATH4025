ed2cf9b243739df3dc5d66f227f44e79e3797947
benj0702 <benj0702@gmail.com>
2018-02-21 15:03:33 -0500

    added a section for trying to correlate derivatives

---
+++ MATLAB/forcing.m
@@ -0,0 +1,27 @@
+BTCXData = 1:length(BTCData);
+
+figure(1451232);clf;hold on; title 'Prices'
+plot(BTCXData, BTCData(:,2)*10^-8);
+plot(firstBTC);
+plot(secondBTC);
+legend('BTC/USD (e-8 corrected)','USD/Coin','USD/Coin');
+
+smoothingFactor = 9;
+BTCUSDDiff = diff(smooth(BTCData(:,2), smoothingFactor));
+BTCUSDDiff(end + 1) = BTCUSDDiff(end);
+cryptoDiff = diff(smooth(firstBTC, smoothingFactor));
+cryptoDiff(end + 1) = cryptoDiff(end);
+
+figure(1234123);clf;hold on; title 'Derivatives';
+plot(BTCUSDDiff);
+plot(cryptoDiff*10^8);
+legend('btc/usd','btc/coin (scaled)');
+
+compEqn = 'a*(x+b)'
+
+cf = fit(cryptoDiff,BTCUSDDiff,compEqn)
+figure(1234656363);clf;
+plot(cf,cryptoDiff,BTCUSDDiff)
+title('curve fit results');
+xlabel('BTC/Coin derivative')
+ylabel('USD/BTC derivative')
--- MATLAB/graphCrypto.m
+++ MATLAB/graphCrypto.m
@@ -17,33 +17,6 @@ secondBTC = secondUSD;
 firstBTC(:,2) = firstBTC(:,2) ./ BTCData(:,2);
 secondBTC(:,2) = secondBTC(:,2) ./ BTCData(:,2);
 
-startPoint = 1570;
-firstBTC = firstBTC(startPoint:end,2);
-secondBTC = secondBTC(startPoint:end,2);
+firstBTC = firstBTC(1:end,2);
+secondBTC = secondBTC(1:end,2);
 
-xData = 1:length(firstBTC);
-
-figure(124355378);clf;hold on; title 'BTC Prices'
-plot(xData,firstBTC);
-plot(xData,secondBTC);
-
-
-    
-%{
-figure(12341234);clf;hold on;
-for i = 1:length(cryptoNames)
-    figure(i);clf;
-    figure(12341234);
-    fileName = [cryptoNames{i}, '.csv'];
-    cryptoData{i} = csvread(fileName)';
-    maxLength = max(maxLength, length(cryptoData{i}));
-    plot(cryptoData{i}(2,:))
-    title([cryptoNames(i), ' price vs. time']);
-end
-
-for i = 1:length(cryptoNames)
-    cryptoData{i}(1,:) = cryptoData{i}(1,:) + maxLength - max(cryptoData{i}(1,:));
-    plot(cryptoData{i}(1,:),cryptoData{i}(2,:));
-end
-
-%}
