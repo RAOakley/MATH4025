6779f17e8999ae57888cca5c5bfb1ab27964cb0f
RAOakley <raoakey@gmail.com>
2018-03-28 14:02:10 -0400

    Got sentiment analysis working!!!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MATH4025
+
+## Required Pacakges
+Run `pip install "keras<2"` or the sentiment analysis won't work
---
+++ twitter-sentiment-analysis-master/predict.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from pprint import pprint
+import sys
+import os
+
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NB_WORDS = 20000
+
+def infer(tweets=[]):
+	os.environ['CUDA_VISIBLE_DEVICES'] = '1'  # str(random.randint(0, 15))
+
+	'''
+	X_raw = []
+	for line in sys.stdin:
+		X_raw.append(line)
+	'''
+
+	X_raw = tweets
+
+	X, word_index = tokenize_data(X_raw)
+
+	model = load_model('model.h5')
+	model.load_weights("weights.h5")
+
+	# Note that each tweet is a 2-tuple: [%-chance negative, %-chance positive]
+	predictions = model.predict(x=X, batch_size=128)
+
+	'''
+	for index, txt in enumerate(X_raw):
+		is_positive = predictions[index][1] >= 0.5
+		status_txt = "Positive" if is_positive else "Negative"
+		print("[",status_txt,"] ", txt)
+	'''
+	pprint(predictions)
+	return predictions
+
+def main():
+	inpts = []
+	argvs = sys.argv.copy()
+	argvs.pop(0)
+
+	for arg in argvs:
+		inpts.append(arg)
+
+	infer(tweets=inpts)
+	print(inpts)
+
+	return
+
+def tokenize_data(X_raw):
+	tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+	tokenizer.fit_on_texts(X_raw)
+	sequences = tokenizer.texts_to_sequences(X_raw)
+	word_index = tokenizer.word_index
+	X_processed = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+	return X_processed, word_index
+
+
+if __name__ == "__main__":
+	main()
